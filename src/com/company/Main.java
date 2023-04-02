1b75f79242c3daabf2b6459665f3bf825f834f93
IvanStoikov2004 <ivan13.is@abv.bg>
2023-04-02 23:30:47 +0300

    commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+import java.math.*;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input=new Scanner(System.in);
+
+        int a=input.nextInt();
+        System.out.println(Math.pow(a,2));
+
     }
 }
